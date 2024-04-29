ba8b7ead0d3d0cbd3c11c640a2b9997388f85ffe
Yuanzhe Yang <vinceHello@outlook.com>
2024-04-29 15:29:34 +0100

    phase 2 finish process delete

--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -15,130 +15,182 @@ import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EDAAppStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    //---------------------------------------------------------------------------
+        //---------------------------------------------------------------------------
 
-    // phase 1
-    const imagesBucket = new s3.Bucket(this, "images", {
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-      publicReadAccess: false,
-    });
+        // phase 1
+        const imagesBucket = new s3.Bucket(this, "images", {
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
+            publicReadAccess: false,
+        });
 
-    const topic1 = new sns.Topic(this, "topic1", {
-      displayName: "topic 1",
-    });
-    imagesBucket.addEventNotification(
-        s3.EventType.OBJECT_CREATED,
-        new s3n.SnsDestination(topic1)
-    );
+        const topic1 = new sns.Topic(this, "topic1", {
+            displayName: "topic 1",
+        });
+        imagesBucket.addEventNotification(
+            s3.EventType.OBJECT_CREATED,
+            new s3n.SnsDestination(topic1)
+        );
 
-    const confirmationMailerFn = new lambdanode.NodejsFunction(
-        this,
-        "ConfirmationMailerFn",
-        {
-          runtime: lambda.Runtime.NODEJS_18_X,
-          entry: `${__dirname}/../lambdas/confirmationMailer.ts`,
-          timeout: cdk.Duration.seconds(15),
-          memorySize: 128,
-        }
-    );
-    topic1.addSubscription(new subs.LambdaSubscription(confirmationMailerFn));
+        const confirmationMailerFn = new lambdanode.NodejsFunction(
+            this,
+            "ConfirmationMailerFn",
+            {
+                runtime: lambda.Runtime.NODEJS_18_X,
+                entry: `${__dirname}/../lambdas/confirmationMailer.ts`,
+                timeout: cdk.Duration.seconds(15),
+                memorySize: 128,
+            }
+        );
+        topic1.addSubscription(new subs.LambdaSubscription(confirmationMailerFn));
 
-    confirmationMailerFn.addToRolePolicy(
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          actions: [
-            "ses:SendEmail",
-            "ses:SendRawEmail",
-            "ses:SendTemplatedEmail",
-          ],
-          resources: ["*"],
-        })
-    );
+        confirmationMailerFn.addToRolePolicy(
+            new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: [
+                    "ses:SendEmail",
+                    "ses:SendRawEmail",
+                    "ses:SendTemplatedEmail",
+                ],
+                resources: ["*"],
+            })
+        );
 
-    const DLQ = new sqs.Queue(this, "bad-orders-q", {
-      retentionPeriod: Duration.minutes(30),
-    });
+        const DLQ = new sqs.Queue(this, "bad-orders-q", {
+            retentionPeriod: Duration.minutes(30),
+        });
 
-    const rejectionMailerFn = new lambdanode.NodejsFunction(
-        this,
-        "RejectionMailerFn",
-        {
-          runtime: lambda.Runtime.NODEJS_18_X,
-          entry: `${__dirname}/../lambdas/rejectionMailer.ts`,
-          timeout: cdk.Duration.seconds(15),
-          memorySize: 128,
-        }
-    );
-    rejectionMailerFn.addEventSource(
-        new events.SqsEventSource(DLQ, {
-          batchSize: 5,
-          maxBatchingWindow: cdk.Duration.seconds(10),
-        })
-    );
-    rejectionMailerFn.addToRolePolicy(
-        new iam.PolicyStatement({
-          effect: iam.Effect.ALLOW,
-          actions: [
-            "ses:SendEmail",
-            "ses:SendRawEmail",
-            "ses:SendTemplatedEmail",
-          ],
-          resources: ["*"],
-        })
-    );
-
-
-    const Queue = new sqs.Queue(this, "orders-queue", {
-      receiveMessageWaitTime: cdk.Duration.seconds(10),
-      deadLetterQueue: {
-        queue: DLQ,
-        maxReceiveCount: 2,
-      },
-    });
-
-    topic1.addSubscription(new subs.SqsSubscription(Queue));
+        const rejectionMailerFn = new lambdanode.NodejsFunction(
+            this,
+            "RejectionMailerFn",
+            {
+                runtime: lambda.Runtime.NODEJS_18_X,
+                entry: `${__dirname}/../lambdas/rejectionMailer.ts`,
+                timeout: cdk.Duration.seconds(15),
+                memorySize: 128,
+            }
+        );
+        rejectionMailerFn.addEventSource(
+            new events.SqsEventSource(DLQ, {
+                batchSize: 5,
+                maxBatchingWindow: cdk.Duration.seconds(10),
+            })
+        );
+        rejectionMailerFn.addToRolePolicy(
+            new iam.PolicyStatement({
+                effect: iam.Effect.ALLOW,
+                actions: [
+                    "ses:SendEmail",
+                    "ses:SendRawEmail",
+                    "ses:SendTemplatedEmail",
+                ],
+                resources: ["*"],
+            })
+        );
 
 
-    const processImageFn = new lambdanode.NodejsFunction(
-        this,
-        "ProcessImageFn",
-        {
-          runtime: lambda.Runtime.NODEJS_18_X,
-          entry: `${__dirname}/../lambdas/processImage.ts`,
-          timeout: cdk.Duration.seconds(15),
-          memorySize: 128,
-        }
-    );
-    imagesBucket.grantRead(processImageFn);
-    processImageFn.addEventSource(
-        new events.SqsEventSource(Queue, {
-          batchSize: 5,
-          maxBatchingWindow: cdk.Duration.seconds(10),
-        })
-    );
+        const Queue = new sqs.Queue(this, "orders-queue", {
+            receiveMessageWaitTime: cdk.Duration.seconds(10),
+            deadLetterQueue: {
+                queue: DLQ,
+                maxReceiveCount: 2,
+            },
+        });
 
-    const imagesTable = new dynamodb.Table(this, "imagesTable", {
-        billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-        partitionKey: { name: "ImageName", type: dynamodb.AttributeType.STRING },
-        removalPolicy: cdk.RemovalPolicy.DESTROY,
-        stream: dynamodb.StreamViewType.NEW_AND_OLD_IMAGES,
-        tableName: "Images",
-      });
-
-    imagesTable.grantReadWriteData(processImageFn);
+        topic1.addSubscription(new subs.SqsSubscription(Queue));
 
 
-      new cdk.CfnOutput(this, "bucketName", {
-          value: imagesBucket.bucketName,
-      });
-      new cdk.CfnOutput(this, "topic1ARN", {
-          value: topic1.topicArn,
-      });
+        const processImageFn = new lambdanode.NodejsFunction(
+            this,
+            "ProcessImageFn",
+            {
+                runtime: lambda.Runtime.NODEJS_18_X,
+                entry: `${__dirname}/../lambdas/processImage.ts`,
+                timeout: cdk.Duration.seconds(15),
+                memorySize: 128,
+            }
+        );
+        imagesBucket.grantRead(processImageFn);
+        processImageFn.addEventSource(
+            new events.SqsEventSource(Queue, {
+                batchSize: 5,
+                maxBatchingWindow: cdk.Duration.seconds(10),
+            })
+        );
 
-  }
+        const imagesTable = new dynamodb.Table(this, "imagesTable", {
+            billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+            partitionKey: { name: "ImageName", type: dynamodb.AttributeType.STRING },
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            stream: dynamodb.StreamViewType.NEW_AND_OLD_IMAGES,
+            tableName: "Images",
+        });
+
+        imagesTable.grantReadWriteData(processImageFn);
+
+        //------------
+
+
+
+        const topic2 = new sns.Topic(this, "topic2", {
+            displayName: "topic 2",
+        });
+        imagesBucket.addEventNotification(
+            s3.EventType.OBJECT_REMOVED,
+            new s3n.SnsDestination(topic2)
+        );
+
+        const processDeleteFn = new lambdanode.NodejsFunction(
+            this,
+            "ProcessDeleteFn",
+            {
+                runtime: lambda.Runtime.NODEJS_18_X,
+                entry: `${__dirname}/../lambdas/processDelete.ts`,
+                timeout: cdk.Duration.seconds(15),
+                memorySize: 128,
+            }
+        );
+        topic2.addSubscription(new subs.LambdaSubscription(processDeleteFn))
+        imagesTable.grantReadWriteData(processDeleteFn)
+
+
+        //2 Lambda
+        /**
+        const updateTableFn = new lambdanode.NodejsFunction(
+            this,
+            "updateTableFn",
+            {
+                runtime: lambda.Runtime.NODEJS_18_X,
+                entry: `${__dirname}/../lambdas/updateTable.ts`,
+                timeout: cdk.Duration.seconds(15),
+                memorySize: 128,
+            }
+        );
+        topic2.addSubscription(new subs.LambdaSubscription(updateTableFn, {
+            filterPolicy: {
+                comment_type: sns.SubscriptionFilter.stringFilter({
+                    allowlist: ['UpdateTable']
+                }),
+            }
+        }))
+
+        imagesTable.grantReadWriteData(updateTableFn);
+         */
+
+
+        new cdk.CfnOutput(this, "bucketName", {
+            value: imagesBucket.bucketName,
+        });
+        new cdk.CfnOutput(this, "topic1ARN", {
+            value: topic1.topicArn,
+        });
+
+        new cdk.CfnOutput(this, "topic2ARN", {
+            value: topic2.topicArn,
+        });
+
+    }
 }
-
